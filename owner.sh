5aaa2177e2261407fafba78d0b7f0ca35d1c4146
OmaimaIsmail <37181165+OmaimaIsmail@users.noreply.github.com>
2018-06-13 03:42:33 +0200

    Update owner.sh

--- owner.sh
+++ owner.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 chown -R mysql:mysql /var/lib/mysql
-#rm /var/lib/mysql/auto.cnf
 service mysql restart
 mysql -u root -e 'ALTER USER '\'root\''@'\'localhost\'' IDENTIFIED BY '\'1234\'';' -e 'create database wordpress;' -e 'CREATE USER wordpressuser@localhost IDENTIFIED BY '\'1234\'';' -e 'GRANT ALL PRIVILEGES ON wordpress.* TO wordpressuser@localhost;' -e 'FLUSH PRIVILEGES;' 
-#a2enmod rewrite
 service nginx restart
 service php7.0-fpm start
 
